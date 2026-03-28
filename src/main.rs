b1bf5a93176952a034467987edd53f488ac5003a
Daria-1303 <daria.ciobanu@student.upt.ro>
2026-03-29 00:38:18 +0200

    idk ceva web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, Rust world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // Definirea rutei principale
+    let app = Router::new().route("/", get(|| async { "Hello World\n" }));
+
+    // Pornirea serverului
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    
+    println!("Server running on http://127.0.0.1:3000");
+    axum::serve(listener, app).await.unwrap();
 }
